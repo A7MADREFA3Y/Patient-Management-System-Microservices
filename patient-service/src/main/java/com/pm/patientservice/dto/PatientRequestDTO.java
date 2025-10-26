11a0100ff1afd70df2dac5d4ab6313b349e960f4
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-10-26 14:49:12 +0100

    created PatientRequestDto

---
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
@@ -0,0 +1,31 @@
+package com.pm.patientservice.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PatientRequestDTO {
+
+    @NotBlank (message = "Name can't be empty ")
+    @Size (max = 100, message = "Name can't exceed 100 characters")
+    private String name;
+
+    @NotBlank (message = "Name can't be empty ")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Address is required")
+    private String address;
+
+    @NotBlank(message = "Date of birth is required")
+    private String dateOfBirth;
+
+    @NotNull (message = "Registered date is required")
+    private String registeredDate;
+
+}
