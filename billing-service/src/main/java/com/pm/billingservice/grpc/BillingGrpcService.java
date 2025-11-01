a6e1af9ff82e8e5ee21d24c98e2cfecb0390a032
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-11-01 22:20:06 +0100

    billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java

--- billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -4,14 +4,20 @@ package com.pm.billingservice.grpc;
 import billing.BillingsServiceGrpc.BillingsServiceImplBase;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @GrpcService
 public class BillingGrpcService extends BillingsServiceImplBase {
+
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
 
     @Override
     public void createBillingAccount(billing.BillingRequest billingRequest,
                                      StreamObserver<billing.BillingResponse> responseStreamObserver){
 
-    }
+        log.info("createBillingAccount request received {}", billingRequest.toString());
 
+        // Business logic - e.g save to database, perform calculates etc
+    }
 }
