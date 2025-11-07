b51a7d3eb85ac18ef591b8387ac60723d8bf51d0
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-11-07 20:25:14 +0100

    added the service anot.

--- patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -1,15 +1,18 @@
 package com.pm.patientservice.kafka;
 
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
-public class Producer {
+
+@Service
+public class KafkaProducer {
 
     private final KafkaTemplate<String, byte[]> kafkaTemplate;
 
 
-    public Producer(KafkaTemplate<String, byte[]> kafkaTemplate) {
+    public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-     
+
 }
