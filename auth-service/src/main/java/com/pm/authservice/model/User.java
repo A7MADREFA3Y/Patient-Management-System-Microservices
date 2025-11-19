3d13c15474e904776ebb9732e6ef6f397ff4ebf2
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-11-19 20:54:37 +0100

    complete the user model

--- auth-service/src/main/java/com/pm/authservice/model/User.java
+++ auth-service/src/main/java/com/pm/authservice/model/User.java
@@ -1,10 +1,22 @@
 package com.pm.authservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.UUID;
 
 @Entity
 @Table(name = "users")
 public class User {
-        
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
 }
