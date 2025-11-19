026bdbc0834cc6a6d09798e820fec8517a82e1ae
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-11-19 21:04:50 +0100

    added docker file to the auth service

--- auth-service/src/main/java/com/pm/authservice/model/User.java
+++ auth-service/src/main/java/com/pm/authservice/model/User.java
@@ -19,4 +19,37 @@ public class User {
 
     @Column(nullable = false)
     private String role;
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
 }
